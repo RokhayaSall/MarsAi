1a754b7356670e6d97ac718a280019fedc3d7992
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-02-03 14:33:14 +0100

    header terminer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/layout/Navbar';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+// import FormDirector from './pages/FormDirector';
+// import Registration from './pages/Register';
+// import Login from './pages/Login';
+
+import './App.css';
 
 function App() {
-
-
-
   return (
-    <>
-     
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/gallery" element={<Movies />} />
+        {/* <Route path='/form-director' element={<FormDirector/>} />
+      <Route path='/registration' element={<Registration/>} />
+      <Route path='/login' element={<Login/>} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
