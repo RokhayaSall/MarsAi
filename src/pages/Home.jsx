87d8b473a1fcfa72f91adea42f3025c065b59c42
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-02-06 13:44:22 +0100

    Optimisation code accueil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,8 @@
-import ButtonParticipate from '../components/ui/Button.jsx';
-import { MdLocalMovies } from 'react-icons/md';
-import { BsPersonArmsUp } from 'react-icons/bs';
-import { GrUserExpert } from 'react-icons/gr';
-import { IoBook } from 'react-icons/io5';
-import Card from '../components/ui/Card.jsx';
+import { ButtonParticipate, ButtonMore } from '../components/ui/Buttons.jsx';
+import { Card } from '../components/ui/Card.jsx';
+import { cardsData } from '../components/ui/CardsData.jsx';
 
 function Home() {
-  const card = [
-    {
-      id: 1,
-      icon: MdLocalMovies,
-      title: '1 MINUTE',
-      text: "Format ultra-court pour maximiser l'impact créatif.",
-    },
-    {
-      id: 2,
-      icon: IoBook,
-      title: 'GRATUITÉ',
-      text: 'Conférences et workshops accessibles à tous.',
-    },
-    {
-      id: 4,
-      icon: BsPersonArmsUp,
-      title: 'POUR TOUS',
-      text: 'Professionnels, étudiants et curieux.',
-    },
-    {
-      id: 5,
-      icon: GrUserExpert,
-      title: 'EXPERTISE',
-      text: "Rencontrez les leaders mondiaux de l'IA.",
-    },
-  ];
-
   return (
     <>
       <section className="relative text-center bg-[url(src/assets/sunset.jpg)] bg-cover bg-center bg-gray-500 bg-blend-multiply p-10 pt-30 pb-30 md:p-45">
@@ -46,17 +16,12 @@ function Home() {
           d&apos;immersion au cœur de Marseille.
         </p>
         <ButtonParticipate />
-
-        <button className="bg-[#FFFFFF]  rounded-4xl p-4 px-10  font-bold cursor-pointer text-2xl m-10 md:p-3 md:px-5 md:m-1 md:text-lg ">
-          En savoir plus
-          <span className="text-[#ff5845] text-2xl md:text-xl "> +</span>
-        </button>
+        <ButtonMore />
       </section>
 
       <section className="p-20 md:p-15 md:pt-20 bg-[#EFEFEF] ">
-        <h2 className="font-bold text-4xl w-50 mb-5 md:text-5xl md:w-max">
-          {' '}
-          LE PROJET MARS.A.I{' '}
+        <h2 className="font-bold text-4xl w-50 mb-5 text-[#282828] md:text-5xl md:w-max">
+          LE PROJET MARS.A.I
         </h2>
         <p className="w-80 md:w-200">
           Un événement hybride unique en France, réunissant la fine fleur de
@@ -67,7 +32,7 @@ function Home() {
           className="flex flex-col items-center gap-6 mt-8 mb-8 md: md:flex-row justify-around  "
           aria-label="Description projet MarsAI"
         >
-          {card.map(card => (
+          {cardsData.map(card => (
             <Card
               key={card.id}
               icon={card.icon}
