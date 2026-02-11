3698abbd077a03e18e8d6d66c078819e0d1d55be
Alexandre-Ck <alexandre.chikhaoui@laplateforme.io>
2026-02-11 09:54:09 +0100

    design composants cards modifiés

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,12 @@ import Movies from './pages/Movies';
 import FormDirector from './pages/FormDirector';
 import SubmitMovie from './pages/SubmitMovie';
 import Auth from './pages/Auth';
-import DashbordAdmin from './pages/DashbordAdminPage/DashbordAdmin';
+import DashboardAdmin from './pages/DashbordAdminPage/DashbordAdmin';
 import AdminJury from './pages/DashbordAdminPage/AdminJury';
 import AdminConfig from './pages/DashbordAdminPage/AdminConfig';
 import AdminEvents from './pages/DashbordAdminPage/AdminEvents';
 import AdminMovies from './pages/DashbordAdminPage/AdminMovies';
 import AdminMoviesResult from './pages/DashbordAdminPage/AdminMoviesResult';
-
-// import AdminFilms from './pages/AdminFilms';
-// import AdminJury from './pages/AdminJury';
-// import AdminCountries from './pages/AdminCountries';
-// import AdminWorkshops from './pages/AdminWorkshops';
-// import AdminDirectors from './pages/AdminDirectors';
 
 import './App.css';
 
@@ -27,22 +21,19 @@ function App() {
       <Header />
       <main>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/gallery" element={<Movies />} />
           <Route path="/form-director" element={<FormDirector />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/admin" element={<DashbordAdmin />} />
+          <Route path="/submitMovie" element={<SubmitMovie />} />
+
+          <Route path="/admin" element={<DashboardAdmin />} />
           <Route path="/admin/jury" element={<AdminJury />} />
           <Route path="/admin/config" element={<AdminConfig />} />
           <Route path="/admin/events" element={<AdminEvents />} />
           <Route path="/admin/films" element={<AdminMovies />} />
           <Route path="/admin/results" element={<AdminMoviesResult />} />
 
-          {/* <Route path="/admin/notefilms" element={<AdminFilms />} />
-          <Route path="/admin/notejury" element={<AdminJury />} />
-          <Route path="/admin/countries" element={<AdminCountries />} />
-          <Route path="/admin/workshops" element={<AdminWorkshops />} />
-          <Route path="/admin/accountrealisateurs" element={<AdminDirectors />} /> */}
           <Route path="*" element={<Home />} />
         </Routes>
       </main>
