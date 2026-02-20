76c01809c2a1d34e12455bd2a6146df261fa0248
Alexandre-Ck <alexandre.chikhaoui@laplateforme.io>
2026-02-20 16:12:29 +0100

    fichier .env enlevé de github

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -21,7 +21,7 @@
 //   return data;
 // };
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
 
 export const apiFetch = async (url, options = {}) => {
   const token = localStorage.getItem('token');
