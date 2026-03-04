b4684034fbed2035039daf7f18eed5f7b6c12fb3
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-03-04 10:28:31 +0100

    fichier dashboardjury déplacé dans le dossier dashboardjury

---
+++ src/pages/DashboardJuryPage/DashboardJury.jsx
@@ -0,0 +1,10 @@
+function DashboardJury() {
+  window.scrollTo(0, 0);
+  return (
+    <section>
+      <h1 className="m-5 text-5xl">Dashboard jury</h1>
+    </section>
+  );
+}
+
+export default DashboardJury;
